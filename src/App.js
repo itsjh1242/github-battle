c563cf80989184d813ce8d6cfcda32a9bf52cd53
itsjh <93984898+itsjh1242@users.noreply.github.com>
2024-03-04 02:55:16 +0900

    docs .env

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import AnalysisDetail from "./components/AnalysisDetail";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.REACT_APP_PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/analysis" element={<Analysis />} />
