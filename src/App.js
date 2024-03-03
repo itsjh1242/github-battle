0b0d3d352e6b61ac1a07bc73d3a83f08c75bc6cf
itsjh <93984898+itsjh1242@users.noreply.github.com>
2024-03-04 03:01:41 +0900

    docs public url

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import AnalysisDetail from "./components/AnalysisDetail";
 function App() {
   return (
     <>
-      <BrowserRouter basename={process.env.REACT_APP_PUBLIC_URL}>
+      <BrowserRouter basename="https://itsjh1242.github.io/github_battle">
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/analysis" element={<Analysis />} />
