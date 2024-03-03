2e4a80d109327ac8e489dabc6378eea382813414
itsjh <93984898+itsjh1242@users.noreply.github.com>
2024-03-04 03:27:45 +0900

    edit HashRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 //25.03 토큰 만료
 
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 // Components
 import Landing from "./components/Landing";
@@ -11,13 +11,13 @@ import AnalysisDetail from "./components/AnalysisDetail";
 function App() {
   return (
     <>
-      <BrowserRouter basename="https://itsjh1242.github.io/github_battle">
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/analysis" element={<Analysis />} />
           <Route path="/analysis/detail/:user" element={<AnalysisDetail />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
